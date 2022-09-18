536347d8078cbb48556269bdeccfc540e84fee81
LRJ <3502153412@qq.com>
2022-09-18 17:44:15 +0800

    first test commit

---
+++ src/main/java/com/lrj/app/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.lrj.app;
+
+/**
+ * @author <a href="mailto:3502153412@qq.com">LRJ</a>
+ * @version jdk1.8
+ * @CreateDate 2022/9/18T17:39
+ * @ProjectDetails Hello, Git
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git");
+    }
+}
---
+++ src/main/java/com/lrj/app/Test.java
@@ -0,0 +1,10 @@
+package com.lrj.app;
+
+/**
+ * @author <a href="mailto:3502153412@qq.com">LRJ</a>
+ * @version jdk1.8
+ * @CreateDate T
+ * @ProjectDetails Test
+ */
+public class Test {
+}
