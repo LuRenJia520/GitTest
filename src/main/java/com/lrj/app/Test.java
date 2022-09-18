209e9c5678c0bf0a3a68e32de1c0a7036af43e01
LRJ <3502153412@qq.com>
2022-09-18 18:04:23 +0800

    master branch commit test2

--- src/main/java/com/lrj/app/Test.java
+++ src/main/java/com/lrj/app/Test.java
@@ -10,5 +10,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println("hot-fix branch commit test1");
+        System.out.println("hot-fix branch commit test2");
     }
 }
